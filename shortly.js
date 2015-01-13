489336d9aa56f96f83e24cb24bd12daf0e669a72
omarduarte <omarduarte@gmail.com>
2015-01-13 12:31:13 -0800

    pre-lunchtime commit with passport half implemented

--- shortly.js
+++ shortly.js
@@ -13,6 +13,10 @@ var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -28,6 +32,82 @@ app.use(express.static(__dirname + '/public'));
 //for creating sessions
 app.use(cookieParser('this is our secret in cookieParser'));
 app.use(session());
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+/*
+ *
+ *   PASSPORT STUFF (BEGIN)
+ *
+ */
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  util.findById(id).then(function (found) {
+    done(err, found.attributes);
+  });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+
+
+      // findByUsername(username, function(err, user) {
+      //   if (err) { return done(err); }
+      //   if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      //   if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+      //   return done(null, user);
+      // });€€
+
+      util.findByUsername(username).then(function (found) {
+        if(found) {
+          bcrypt.compare(password, found.attributes.password, function(err, result) {
+            if (result) {
+              return done(null, found.attributes);
+            } else {
+              //call a function that lets the user know the username/password combo they tried does note exist
+              console.log('wrong password or user does not exist');
+              return done(null, false, { message: 'Invalid username or password' });
+            }
+          });
+        } else {
+          return done(null, false, { message: 'Invalid username or password' });
+        }
+      });
+
+    });
+  }
+));
+
+/*
+ *
+ *   PASSPORT STUFF (END)
+ *
+ */
 
 
 function restrict(req, res, next) {
@@ -166,6 +246,9 @@ app.post('/login', function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 //talk through how we would implement everything in extra credit and nightmare modes
+//1. Sessions manually
+//2. database stuff- each user gets their own links
+//3. oauth- log in with github credentials
 //sessions:
   //time to live for tokens
   //saving tokens in database along with their expiration time
